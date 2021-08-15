83bc6bc05a82275f8d9fb4d068e07b7293c6239c
Rob Passmore <robertmpassmore@gmail.com>
2021-08-15 10:41:53 +0100

    Redirect to index after update

--- spec/requests/item_flow_spec.rb
+++ spec/requests/item_flow_spec.rb
@@ -6,4 +6,11 @@ RSpec.describe 'Item flow' do
 
     expect(response).to redirect_to(items_path)
   end
+
+  it 'redirects to the index view after successful updatign of an item' do
+    post '/items', params: { item: { description: "clean my room", complete: false }}
+    patch item_url(assigns(:item)), params: { item: { complete: true }}
+
+    expect(response).to redirect_to(items_path)
+  end
 end
